29f43adb7e13b007643df9acae844b6c17712cbd
yibana <lovefy.eth@gmail.com>
2023-08-11 04:32:31 +0800

    contracts

--- contracts/Seaport1_5/Seaport1_5Interface.go
+++ contracts/Seaport1_5/Seaport1_5Interface.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // AdditionalRecipient is an auto generated low-level Go binding around an user-defined struct.
@@ -271,11 +272,11 @@ func NewSeaport15InterfaceFilterer(address common.Address, filterer bind.Contrac
 
 // bindSeaport15Interface binds a generic wrapper to an already deployed contract.
 func bindSeaport15Interface(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(Seaport15InterfaceABI))
+	parsed, err := Seaport15InterfaceMetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
